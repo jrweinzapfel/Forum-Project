96e3a66a2c8aa7e7128be3a28e55e42b2cfdd4c1
Jules Weinzapfel <jrweinzapfel@gmail.com>
2025-08-07 16:02:52 -0500

    reinstalled dependencies and adjusted for aws db

--- dist/prisma.js
+++ dist/prisma.js
@@ -1,3 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import env from 'dotenv';
+env.config();
+console.log('conn string', process.env.Database_URL);
 let prisma = new PrismaClient();
 export default prisma;
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import env from 'dotenv'
+env.config();
+console.log('conn string', process.env.Database_URL);
 
 let prisma = new PrismaClient();
 
